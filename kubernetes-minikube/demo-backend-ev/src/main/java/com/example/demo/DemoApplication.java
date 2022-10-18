70d7103a56fe62176b98321353a8e58fc348b3f4
Dalglish Fernandes <dalglish.fernandes@sainsburys.co.uk>
2022-10-18 11:43:34 +0100

    Demo project

---
+++ kubernetes-minikube/demo-backend-ev/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	 static void myMethod() {
+		    System.out.println("I just got executed!");
+	}
+	 public static void main(String[] args) {
+		    myMethod();
+	}
+}
+
+
---
+++ kubernetes-minikube/demo-backend-ev/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class HelloController {
+	  static void myMethod() {
+		    System.out.println("I just got executed!");
+	}
+	 public static void main(String[] args) {
+		    myMethod();
+	}
+}
